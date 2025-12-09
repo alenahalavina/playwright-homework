226ed6d5e8f9f9df445e7309a53d411e7c1162db
Alena Halavina <alenahalavin@gmail.com>
2025-12-08 16:29:34 -0800

    Updated Dialog boxes based on review

--- tests/dialogBoxes.spec.ts
+++ tests/dialogBoxes.spec.ts
@@ -10,15 +10,17 @@ await page.getByText('PET TYPES').click()
 await expect(page.getByRole('heading')).toHaveText('Pet Types')
 await page.getByRole('button', { name: 'Add' }).click();
 await expect(page.getByRole('heading', { name: 'New Pet Type' })).toBeVisible();
-await expect(page.locator('form').locator('label').filter({ hasText: 'Name' })).toBeVisible();
+await expect(page.locator('form label', {hasText: 'Name'})).toBeVisible();
 await expect(page.locator('#name')).toBeVisible();
 await page.locator('#name').fill('pig');
 await page.getByRole('button', { name: 'Save' }).click();
 const petTypes = page.locator('input[name="pettype_name"]');
 await expect(petTypes.last()).toHaveValue('pig');
 
+
 page.on('dialog', dialog => {
-    dialog.accept()
+  expect(dialog.message()).toEqual('Delete the pet type?')
+  dialog.accept()
 });
 
 await page.locator('table tr').last().getByRole('button', { name: 'Delete' }).click();
